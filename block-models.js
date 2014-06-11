bfd44efaf3f5fb751ee7d9936c0dcdded3e8590f
deathcap <deathcap@gmx.co.uk>
2014-06-10 20:46:27 -0700

    cubePositions

--- block-models.js
+++ block-models.js
@@ -1,13 +1,13 @@
 'use strict';
 
-module.exports = function() {
-
-  var from = [0,0,0];
-  var to = [16,16,16];
-
-  var a = from;
-  var b = to;
-  var positions = [
+// get coordinates for a cube ranging from vertex a to b
+//   ____
+//  /   /|
+// a---+ |
+// |   | b
+// +---+/
+var cubePositions = function(a, b) {
+  return [
     [a[0]       , a[1]       , a[2]       ], // 0
     [a[0]       , a[1]       , a[2] + b[2]], // 1
     [a[0]       , a[1] + b[1], b[2]       ], // 2
@@ -17,6 +17,13 @@ module.exports = function() {
     [a[0] + b[1], a[1] + b[1], b[2]       ], // 6
     [a[0] + b[1], a[1] + b[1], b[2] + b[2]], // 7
   ];
+};
+
+module.exports = function() {
+  var from = [0,0,0];
+  var to = [16,16,16];
+
+  var positions = cubePositions(from, to);
 
   var cells = [
     [0,1,7],
