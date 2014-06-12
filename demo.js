1ecd9f5bb838dedd737c1333a104d93e8faf23f4
deathcap <deathcap@gmx.co.uk>
2014-06-11 21:22:30 -0700

    Start converting to use planes

--- demo.js
+++ demo.js
@@ -7,37 +7,12 @@ var createContext = require('gl-context')
 var glslify       = require('glslify')
 
 var createBlockGeometry = require('./')
+var planeUtils = require('./gl-plane.js')
 
 var canvas     = document.body.appendChild(document.createElement('canvas'))
 var gl         = createContext(canvas, render)
 var camera     = createCamera(canvas)
 var projection = mat4.create()
-var shader     = glslify({
-    inline: true,
-    vert: '\
-precision mediump float;\
-\
-attribute vec3 position;\
-uniform mat4 uProjection;\
-uniform mat4 uView;\
-uniform mat4 uModel;\
-\
-void main() {\
-\
-gl_Position = (\
-    uProjection\
-  * uView\
-  * uModel\
-  * vec4(position, 1.0)\
-);\
-}'
-  , frag: '\
-precision mediump float;\
-\
-void main() {\
-gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);\
-}'
-})(gl)
 
 canvas.width = 512
 canvas.height = 512
@@ -79,8 +54,20 @@ window.setInterval(function() {
 }, 200)
 
 var modelMatrix = mat4.create()
-var s = 5
+var s = 2
 mat4.scale(modelMatrix, modelMatrix, [s,s,s])
+
+var shader = planeUtils.createPlaneShader(gl)
+var mesh = planeUtils.createPlaneMesh(gl,
+    // TODO: replace with createBlockGeometry
+  [
+    {position:[0,0,0], normal:[-1,0,0], texture:'furnace_top'},
+    {position:[0,1,0], normal:[+1,0,0], texture:'furnace_top'},
+    {position:[0,2,0], normal:[0,+1,0], texture:'furnace_top'},
+    {position:[0,3,0], normal:[0,-1,0], texture:'furnace_top'},
+    {position:[0,4,0], normal:[0,0,+1], texture:'furnace_front_on'},
+    {position:[0,5,0], normal:[0,0,-1], texture:'furnace_top'},
+  ])
 
 function render() {
   var width  = canvas.width
@@ -91,19 +78,22 @@ function render() {
   gl.enable(gl.DEPTH_TEST)
   gl.viewport(0, 0, width, height)
 
-  geom.bind(shader)
+  shader.bind()
   shader.attributes.position.location = 0
-  shader.uniforms.uView = camera.view()
-  shader.uniforms.uModel = modelMatrix
-  shader.uniforms.uProjection = mat4.perspective(projection
+  shader.uniforms.view = camera.view()
+  shader.uniforms.model = modelMatrix
+  shader.uniforms.projection = mat4.perspective(projection
     , Math.PI / 4
     , width / height
     , 0.001
     , 10000
   )
+  // use same atlas from voxel-shader TODO: can we reliably avoid binding? if already bound, seems to reuse
+  //if (this.stitchPlugin.texture) this.shader.uniforms.texture = this.stitchPlugin.texture.bind();
 
-  geom.draw()
-  geom.unbind()
+  mesh.bind();
+  mesh.draw(gl.TRIANGLES, mesh.length);
+  mesh.unbind();
 
   camera.tick()
 }
