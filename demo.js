31f8ae1389e2a396ce8dcd9134b283c36d53563e
deathcap <deathcap@gmx.co.uk>
2014-06-11 18:26:35 -0700

    Add live JSON editing to demo

--- demo.js
+++ demo.js
@@ -8,7 +8,52 @@ var glslify       = require('glslify')
 
 var createBlockGeometry = require('./')
 
-showExample(
+var canvas     = document.body.appendChild(document.createElement('canvas'))
+var gl         = createContext(canvas, render)
+var camera     = createCamera(canvas)
+var projection = mat4.create()
+var shader     = glslify({
+    inline: true,
+    vert: '\
+precision mediump float;\
+\
+attribute vec3 position;\
+attribute vec3 normal;\
+varying vec3 vnormal;\
+uniform mat4 uProjection;\
+uniform mat4 uView;\
+uniform mat4 uModel;\
+\
+void main() {\
+vnormal = (uView * vec4(normal, 1.0)).xyz / 2.0 + 0.5;\
+\
+gl_Position = (\
+    uProjection\
+  * uView\
+  * uModel\
+  * vec4(position, 1.0)\
+);\
+}'
+  , frag: '\
+precision mediump float;\
+\
+varying vec3 vnormal;\
+\
+void main() {\
+gl_FragColor = vec4(vnormal, 1.0);\
+}'
+})(gl)
+
+canvas.width = 512
+canvas.height = 512
+canvas.style.margin = '1em'
+canvas.style.border = '1px solid black'
+
+var textarea = document.createElement('textarea')
+textarea.id = 'q'
+textarea.rows = '50'
+textarea.cols = '80'
+var exampleData =
    // example parsed JSON
   [
     {from: [0,0,0],
@@ -21,79 +66,50 @@ showExample(
       west: {},
       east: {}},
     }
-  ])
+  ]
 
-function showExample(data) {
-  var canvas     = document.body.appendChild(document.createElement('canvas'))
-  var gl         = createContext(canvas, render)
-  var camera     = createCamera(canvas)
-  var projection = mat4.create()
-  var shader     = glslify({
-      inline: true,
-      vert: '\
-precision mediump float;\
-\
-attribute vec3 position;\
-attribute vec3 normal;\
-varying vec3 vnormal;\
-uniform mat4 uProjection;\
-uniform mat4 uView;\
-uniform mat4 uModel;\
-\
-void main() {\
-  vnormal = (uView * vec4(normal, 1.0)).xyz / 2.0 + 0.5;\
-\
-  gl_Position = (\
-      uProjection\
-    * uView\
-    * uModel\
-    * vec4(position, 1.0)\
-  );\
-}'
-    , frag: '\
-precision mediump float;\
-\
-varying vec3 vnormal;\
-\
-void main() {\
-  gl_FragColor = vec4(vnormal, 1.0);\
-}'
-  })(gl)
+var oldText = textarea.value = JSON.stringify(exampleData, null, '  ')
+document.body.appendChild(textarea)
 
-  canvas.width = 512
-  canvas.height = 512
-  canvas.style.margin = '1em'
-  canvas.style.border = '1px solid black'
+var geom = createBlockGeometry(gl, exampleData)
 
-  var geom = createBlockGeometry(gl, data)
+document.body.addEventListener('keydown', function() {
+  process.nextTick(function() {
+    var text = textarea.value
+    if (text.length === oldText.length && text === oldText.length) return // no change
 
-  var modelMatrix = mat4.create()
-  var s = 5
-  mat4.scale(modelMatrix, modelMatrix, [s,s,s])
+    var data = JSON.parse(text)
+    geom = createBlockGeometry(gl, data)
+    console.log('updated geometry',geom)
+  })
+})
 
-  function render() {
-    var width  = canvas.width
-    var height = canvas.height
+var modelMatrix = mat4.create()
+var s = 5
+mat4.scale(modelMatrix, modelMatrix, [s,s,s])
 
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null)
-    gl.enable(gl.CULL_FACE)
-    gl.enable(gl.DEPTH_TEST)
-    gl.viewport(0, 0, width, height)
+function render() {
+  var width  = canvas.width
+  var height = canvas.height
 
-    geom.bind(shader)
-    shader.attributes.position.location = 0
-    shader.uniforms.uView = camera.view()
-    shader.uniforms.uModel = modelMatrix
-    shader.uniforms.uProjection = mat4.perspective(projection
-      , Math.PI / 4
-      , width / height
-      , 0.001
-      , 10000
-    )
+  gl.bindFramebuffer(gl.FRAMEBUFFER, null)
+  gl.enable(gl.CULL_FACE)
+  gl.enable(gl.DEPTH_TEST)
+  gl.viewport(0, 0, width, height)
 
-    geom.draw()
-    geom.unbind()
+  geom.bind(shader)
+  shader.attributes.position.location = 0
+  shader.uniforms.uView = camera.view()
+  shader.uniforms.uModel = modelMatrix
+  shader.uniforms.uProjection = mat4.perspective(projection
+    , Math.PI / 4
+    , width / height
+    , 0.001
+    , 10000
+  )
 
-    camera.tick()
-  }
+  geom.draw()
+  geom.unbind()
+
+  camera.tick()
 }
